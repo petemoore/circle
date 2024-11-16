a0206b3a1d68306d245568336026b79947742567
Rene Stange <rsta2@o2online.de>
2024-11-16 09:20:04 +0100

    font: Move to include/circle

--- lib/chargenerator.cpp
+++ lib/chargenerator.cpp
@@ -2,7 +2,7 @@
 // chargenerator.cpp
 //
 // Circle - A C++ bare metal environment for Raspberry Pi
-// Copyright (C) 2014  R. Stange <rsta2@o2online.de>
+// Copyright (C) 2014-2024  R. Stange <rsta2@o2online.de>
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -21,8 +21,8 @@
 static const char From[] = "chargenerator";
 
 #include <circle/chargenerator.h>
+#include <circle/font.h>
 #include <assert.h>
-#include "font.h"
 
 #undef GIMP_HEADER			// if font saved with GIMP with .h extension
 

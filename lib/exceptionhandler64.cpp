daa74314c58d5f0e2b9113a07208a9bfd00fd1ac
Pete Moore <pmoore@mozilla.com>
2024-12-07 15:21:22 +0100

    wip - log arm regs and improve mmio read/write logs

--- lib/exceptionhandler64.cpp
+++ lib/exceptionhandler64.cpp
@@ -29,6 +29,17 @@ static const char From[] = "exceptionhandler64";
 #include <circle/string.h>
 #include <assert.h>
 
+extern "C" unsigned long long cnthctl_el2_val;
+extern "C" unsigned long long cntvoff_el2_val;
+extern "C" unsigned long long cptr_el2_val;
+extern "C" unsigned long long elr_el2_val;
+extern "C" unsigned long long hcr_el2_val;
+extern "C" unsigned long long hstr_el2_val;
+extern "C" unsigned long long spsr_el2_val;
+extern "C" unsigned long long vbar_el2_val;
+extern "C" unsigned long long vmpidr_el2_val;
+extern "C" unsigned long long vpidr_el2_val;
+
 static const char FromExcept[] = "except";
 
 // order must match exception identifiers in circle/exception.h
@@ -54,6 +65,9 @@ CExceptionHandler::~CExceptionHandler (void)
 
 void CExceptionHandler::Throw (unsigned nException, TAbortFrame *pFrame)
 {
+	unsigned long long arm_reg_value;
+	CLogger *logger;
+
 	assert (pFrame != 0);
 
 	u64 sp = pFrame->sp_el0;
@@ -76,7 +90,44 @@ void CExceptionHandler::Throw (unsigned nException, TAbortFrame *pFrame)
 	debug_stacktrace ((u64 *) sp, FromExcept);
 #endif
 
-	CLogger::Get()->Write (FromExcept, LogPanic,
+	logger = CLogger::Get();
+
+	asm volatile ("mrs %0, cpacr_el1" : "=r" (arm_reg_value));
+	logger->Write (FromExcept, LogNotice, "cpacr_el1: %016lx", arm_reg_value);
+
+	asm volatile ("mrs %0, elr_el1" : "=r" (arm_reg_value));
+	logger->Write (FromExcept, LogNotice, "elr_el1: %016lx", arm_reg_value);
+
+	asm volatile ("mrs %0, mair_el1" : "=r" (arm_reg_value));
+	logger->Write (FromExcept, LogNotice, "mair_el1: %016lx", arm_reg_value);
+
+	asm volatile ("mrs %0, sctlr_el1" : "=r" (arm_reg_value));
+	logger->Write (FromExcept, LogNotice, "sctlr_el1: %016lx", arm_reg_value);
+
+	asm volatile ("mrs %0, spsr_el1" : "=r" (arm_reg_value));
+	logger->Write (FromExcept, LogNotice, "spsr_el1: %016lx", arm_reg_value);
+
+	asm volatile ("mrs %0, tcr_el1" : "=r" (arm_reg_value));
+	logger->Write (FromExcept, LogNotice, "tcr_el1: %016lx", arm_reg_value);
+
+	asm volatile ("mrs %0, ttbr0_el1" : "=r" (arm_reg_value));
+	logger->Write (FromExcept, LogNotice, "ttbr0_el1: %016lx", arm_reg_value);
+
+	asm volatile ("mrs %0, vbar_el1" : "=r" (arm_reg_value));
+	logger->Write (FromExcept, LogNotice, "vbar_el1: %016lx", arm_reg_value);
+
+	logger->Write (FromExcept, LogNotice, "cnthctl_el2_val: %016lx", cnthctl_el2_val);
+	logger->Write (FromExcept, LogNotice, "cntvoff_el2_val: %016lx", cntvoff_el2_val);
+	logger->Write (FromExcept, LogNotice, "cptr_el2_val: %016lx", cptr_el2_val);
+	logger->Write (FromExcept, LogNotice, "elr_el2_val: %016lx", elr_el2_val);
+	logger->Write (FromExcept, LogNotice, "hcr_el2_val: %016lx", hcr_el2_val);
+	logger->Write (FromExcept, LogNotice, "hstr_el2_val: %016lx", hstr_el2_val);
+	logger->Write (FromExcept, LogNotice, "spsr_el2_val: %016lx", spsr_el2_val);
+	logger->Write (FromExcept, LogNotice, "vbar_el2_val: %016lx", vbar_el2_val);
+	logger->Write (FromExcept, LogNotice, "vmpidr_el2_val: %016lx", vmpidr_el2_val);
+	logger->Write (FromExcept, LogNotice, "vpidr_el2_val: %016lx", vpidr_el2_val);
+
+	logger->Write (FromExcept, LogPanic,
 		"%s (PC 0x%lX, EC 0x%lX, ISS 0x%lX, FAR 0x%lX, SP 0x%lX, LR 0x%lX, SPSR 0x%lX)",
 		s_pExceptionName[nException],
 		pFrame->elr_el1, nEC, nISS, nFAR, sp, pFrame->x30, pFrame->spsr_el1);

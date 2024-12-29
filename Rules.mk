505f6cf523fd3137bc22a3468d85ab4bf106d514
Pete Moore <pmoore@mozilla.com>
2024-12-29 12:02:52 +0000

    Create .i files

--- Rules.mk
+++ Rules.mk
@@ -141,6 +141,14 @@ endif
 	@echo "  CPP   $@"
 	@$(CPP) $(CPPFLAGS) -c -o $@ $<
 
+%.S.i: %.S
+	@echo "  PRE   $@"
+	@$(CPP) -P -E -x assembler-with-cpp $(CPPFLAGS) -o $@ $<
+
+%.cpp.i: %.cpp
+	@echo "  PRE   $@"
+	@$(CPP) -P -E $(CPPFLAGS) -o $@ $<
+
 %.d: %.S
 	@$(AS) $(AFLAGS) -M -MG -MT $*.o -MT $@ -MF $@ $<
 
@@ -164,7 +172,7 @@ $(TARGET).img: $(OBJS) $(LIBS) $(CIRCLEHOME)/circle.ld
 
 clean:
 	@echo "  CLEAN " `pwd`
-	@rm -f *.d *.o *.a *.elf *.lst *.img *.hex *.cir *.map *~ $(EXTRACLEAN)
+	@rm -f *.d *.o *.a *.i *.elf *.lst *.img *.hex *.cir *.map *~ $(EXTRACLEAN)
 
 ifneq ($(strip $(SDCARD)),)
 install: $(TARGET).img

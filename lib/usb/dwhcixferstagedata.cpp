e8fc8eacfd34ca16a866b149afa3afe74af368a0
Rene Stange <rsta2@o2online.de>
2025-01-03 17:20:18 +0100

    Revert: FIXED: usb/dwhci: SetCompleteOnNAK() in no-hub configurations
    This reverts commit ae00d9d89f7d7ea3a1f66e59b25e08c3a82214ee in a way,
    that its changes can be enabled with the system option USE_NAK_USB_FIX
    only. This is necessary, because this commit leads to lost MIDI events
    with USB MIDI devices on the Raspberry Pi 1-3.
    
    See: #467 #509

--- lib/usb/dwhcixferstagedata.cpp
+++ lib/usb/dwhcixferstagedata.cpp
@@ -2,7 +2,7 @@
 // dwhcixferstagedata.cpp
 //
 // Circle - A C++ bare metal environment for Raspberry Pi
-// Copyright (C) 2014-2024  R. Stange <rsta2@o2online.de>
+// Copyright (C) 2014-2025  R. Stange <rsta2@o2online.de>
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -27,6 +27,7 @@ static const char From[] = "dwhcixferstagedata";
 #include <circle/usb/dwhciframeschediso.h>
 #include <circle/usb/dwhci.h>
 #include <circle/usb/usbhostcontroller.h>
+#include <circle/sysconfig.h>
 #include <circle/logger.h>
 #include <circle/timer.h>
 #include <assert.h>
@@ -521,10 +522,12 @@ u32 CDWHCITransferStageData::GetStatusMask (void) const
 			 | DWHCI_HOST_CHAN_INT_NAK
 			 | DWHCI_HOST_CHAN_INT_NYET;
 	}
+#ifdef USE_NAK_USB_FIX
 	else if (m_pURB->IsCompleteOnNAK ())
 	{
 		nMask |= DWHCI_HOST_CHAN_INT_NAK;
 	}
+#endif
 
 	return nMask;
 }

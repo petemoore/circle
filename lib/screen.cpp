5d947dcbeec4f6281553345d7401b2853dafe7c9
Rene Stange <rsta2@o2online.de>
2025-02-03 13:25:46 +0100

    screen: Create own device name "ttyN"
    Before the device name for the screen device was only created with
    SCREEN_HEADLESS enabled. This was inconsistent. Now the device name is
    always created. The CTerminalDevice member still creates its own device
    name with N = nDisplay + 101.

--- lib/screen.cpp
+++ lib/screen.cpp
@@ -2,7 +2,7 @@
 // screen.cpp
 //
 // Circle - A C++ bare metal environment for Raspberry Pi
-// Copyright (C) 2014-2024  R. Stange <rsta2@o2online.de>
+// Copyright (C) 2014-2025  R. Stange <rsta2@o2online.de>
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -23,6 +23,8 @@ static const char From[] = "screen";
 #include <circle/screen.h>
 #include <circle/devicenameservice.h>
 
+static const char DevicePrefix[] = "tty";
+
 #ifndef SCREEN_HEADLESS
 
 CScreenDevice::CScreenDevice (unsigned nWidth, unsigned nHeight, const TFont &rFont,
@@ -39,6 +41,8 @@ CScreenDevice::CScreenDevice (unsigned nWidth, unsigned nHeight, const TFont &rF
 
 CScreenDevice::~CScreenDevice (void)
 {
+	CDeviceNameService::Get ()->RemoveDevice (DevicePrefix, m_nDisplay+1, FALSE);
+
 	delete m_pTerminal;
 	m_pTerminal = nullptr;
 
@@ -84,13 +88,23 @@ boolean CScreenDevice::Initialize (void)
 		return FALSE;
 	}
 
-	m_pTerminal = new CTerminalDevice (m_pFrameBuffer, m_nDisplay, m_rFont, m_FontFlags);
+	m_pTerminal = new CTerminalDevice (m_pFrameBuffer, m_nDisplay + 100, m_rFont, m_FontFlags);
 	if (!m_pTerminal)
 	{
 		return FALSE;
 	}
 
-	return m_pTerminal->Initialize ();
+	if (!m_pTerminal->Initialize ())
+	{
+		return FALSE;
+	}
+
+	if (!CDeviceNameService::Get ()->GetDevice (DevicePrefix, m_nDisplay+1, FALSE))
+	{
+		CDeviceNameService::Get ()->AddDevice (DevicePrefix, m_nDisplay+1, this, FALSE);
+	}
+
+	return TRUE;
 }
 
 boolean CScreenDevice::Resize (unsigned nWidth, unsigned nHeight)
@@ -183,8 +197,6 @@ void CScreenDevice::Update (unsigned nMillis)
 }
 
 #else	// #ifndef SCREEN_HEADLESS
-
-static const char DevicePrefix[] = "tty";
 
 CScreenDevice::CScreenDevice (unsigned nWidth, unsigned nHeight, const TFont &rFont,
 			      CCharGenerator::TFontFlags FontFlags, unsigned nDisplay)
